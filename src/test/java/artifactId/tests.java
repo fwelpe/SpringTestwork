2eca2c7102774f7946f983ffad635bad664714cf
fwelpe <wheel0@mail.ru>
2019-05-24 22:10:39 +0300

    minor fixes

--- src/test/java/artifactId/tests.java
+++ src/test/java/artifactId/tests.java
@@ -5,14 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -34,7 +30,7 @@ public class tests {
     }
 
     @Test
-    public void messageListTest() throws Exception {
+    public void itemListTest() throws Exception {
         this.mockMvc.perform(get("/main"))
                 .andDo(print())
                 .andExpect(xpath("//*[@id='tbl']").nodeCount(1));
